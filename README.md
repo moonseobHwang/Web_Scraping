a9a11a4e947ece4b0fb001fb9a2cca024ec423f6
moon <aa6271235@naver.com>
2020-10-15 09:07:07 +0900

    master

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # New_Develop
-■ 작성자, 동기 또는 목적<br>
-작성자 : 황문섭, 웹스크래핑,셀레니움 훈련 <br>
-■ 적용 기술과 사용법<br>
-셀레니움만으로 스크래핑 구현<br>
 
+황문섭<br>
+
+# job_scraping
+
+web scrapping, DB 저장, Web framework 학습 목적 프로젝트
+
+## 적용 기술
+* Web Server
+    * Django
+    * CSS
+    * HTML
+* Database
+    * MongoDB
+* Web scrapping
+    * BeautifulSoup
+    * selenium
 <br>
 <img src="./datas/FFA66583-87E3-493D-B72C-1DE73CE96593.png" width="500">
 
@@ -11,7 +23,7 @@
 ./datas/scrapjng.py soup 방식<br>
 <br>
 <br>
-■ 간략 화면 설명<br>
+## 간략 화면 설명
 홈 화면 <br>
 <img src="./datas/1.png" width="800"><br>
 <br>
--- datas/scrapjng.py
+++ datas/scrapjng.py
@@ -5,44 +5,51 @@ import requests, time, bs4, sqlite3, schedule
 url = "https://job.incruit.com/entry/searchjob.asp?schol=50&occ1=120&occ1=150"
 header = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
 db_url='mongodb://13.125.191.54:53368'
-#MongoClient(db_url)['workbs4'].sampleCollection.drop()
+MongoClient(db_url)['workbs4'].sampleCollection.drop()
 
 res = requests.get(url)
 soup = BeautifulSoup(res.text, "html5lib")
+def jobprocess():
+    for j in range(1, 2):
+        now = time.gmtime(time.time())
+        print(now.tm_hour,"시", now.tm_min, "분", now.tm_sec,"초")
+        with MongoClient(db_url) as client:
+            workbs4 = client['workbs4']
+            MongoClient(db_url)['workbs4'].sampleCollection.drop()
+            for i in range(1, 30):
+                #incruit_contents > div.section_layout > div.n_job_list_default > div.n_job_list_table_a.list_full_default > table > tbody > tr:nth-child(2)
+                title_data=soup.tbody
+                datas = title_data.select(f"tr:nth-child({i})")
+                for data in datas: # i를 key가앖으로 받아주고 그가앖을 토대로 데이터를 선별해준다
+                    try:
+                        #제목
+                        name_data = data.select_one("td:nth-child(2) > div > span.accent > a")
+                        name_data = str.strip(name_data.get_text())
+                        #지원자격
+                        major_data = data.select_one("td:nth-child(3) > div > p > em")
+                        major_data = str.strip(major_data.get_text())
+                        #근무조건
+                        Condi_data = data.select_one("td:nth-child(4) > div > p > em")
+                        Condi_data = str.strip(Condi_data.get_text())
+                        #마가암일
+                        date_data1 = data.select_one("td.lasts > div.ddays > p:nth-child(2)")
+                        date_data1 = str.strip(date_data1.get_text())
+                        date_data2 = data.select_one("td.lasts > div.mdays > p > span")
+                        date_data2 = str.strip(date_data2.get_text())
+                        date_data = date_data1 + date_data2
+                        #회사명
+                        id_data = data.select_one("th > div > div.check_list_r > span > a")
+                        id_data = str.strip(id_data.get_text())
 
-with MongoClient(db_url) as client:
-    workbs4 = client['workbs4']
-    MongoClient(db_url)['workbs4'].sampleCollection.drop()
-    for i in range(1, 30):
-        #incruit_contents > div.section_layout > div.n_job_list_default > div.n_job_list_table_a.list_full_default > table > tbody > tr:nth-child(2)
-        title_data=soup.tbody
-        datas = title_data.select(f"tr:nth-child({i})")
-        for data in datas: # i를 key가앖으로 받아주고 그가앖을 토대로 데이터를 선별해준다
-            try:
-                #제목
-                name_data = data.select_one("td:nth-child(2) > div > span.accent > a")
-                name_data = str.strip(name_data.get_text())
-                #지원자격
-                major_data = data.select_one("td:nth-child(3) > div > p > em")
-                major_data = str.strip(major_data.get_text())
-                #근무조건
-                Condi_data = data.select_one("td:nth-child(4) > div > p > em")
-                Condi_data = str.strip(Condi_data.get_text())
-                #마가암일
-                date_data1 = data.select_one("td.lasts > div.ddays > p:nth-child(2)")
-                date_data1 = str.strip(date_data1.get_text())
-                date_data2 = data.select_one("td.lasts > div.mdays > p > span")
-                date_data2 = str.strip(date_data2.get_text())
-                date_data = date_data1 + date_data2
-                #회사명
-                id_data = data.select_one("th > div > div.check_list_r > span > a")
-                id_data = str.strip(id_data.get_text())
+                        data={'id_data':id_data, 'name_data':name_data, 'Condi_data':Condi_data, 'major_data':major_data, 'date_data':date_data}
+                        infor = workbs4.sampleCollection.insert_one(data)
+                    except:
+                        pass
+            
+                    print(name_data,major_data,id_data,Condi_data,date_data)
+                time.sleep(0.5)
+schedule.every(180).minutes.do(jobprocess)
 
-                data={'id_data':id_data, 'name_data':name_data, 'Condi_data':Condi_data, 'major_data':major_data, 'date_data':date_data}
-                infor = workbs4.sampleCollection.insert_one(data)
-            except:
-                pass
-    
-            print(name_data,major_data,id_data,Condi_data,date_data)
-
-        time.sleep(0.5)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
